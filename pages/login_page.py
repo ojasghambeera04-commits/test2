67ecec1a422478e55b54fc5530d4c1541d275c26
ojasghambeera04-commits <ojasghambeera04@gmail.com>
2025-12-29 22:58:01 +0530

    Creating or updating file pages/login_page.py via GitHubFileCreator tool

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,7 +1,14 @@
 {
   "pages/base_page.py": "",
   "pages/login_page.py": "",
+  "pages/dashboard_page.py": "",
+  "pages/cart_page.py": "",
+  "pages/checkout_page.py": "",
+  "pages/profile_page.py": "",
   "tests/test_login.py": "",
+  "tests/test_search_and_cart.py": "",
+  "tests/test_checkout.py": "",
+  "tests/test_logout.py": "",
   "conftest.py": "",
   "pytest.ini": "",
   "requirements.txt": ""
