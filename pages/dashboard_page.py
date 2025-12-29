9175eb0c40ec0d0707b57ea6d2ecd8ec20e797ca
ojasghambeera04-commits <ojasghambeera04@gmail.com>
2025-12-29 20:12:31 +0530

    Creating or updating file pages/dashboard_page.py via GitHubFileCreator tool

---
+++ pages/dashboard_page.py
@@ -0,0 +1,11 @@
+{
+  "pages/base_page.py": "",
+  "pages/login_page.py": "",
+  "pages/dashboard_page.py": "",
+  "pages/cart_page.py": "",
+  "pages/checkout_page.py": "",
+  "tests/test_login.py": "",
+  "conftest.py": "",
+  "pytest.ini": "",
+  "requirements.txt": ""
+}
