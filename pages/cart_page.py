73e3e578a0750fe0ba951f25b1d2fede5735b015
ojasghambeera04-commits <ojasghambeera04@gmail.com>
2025-12-29 22:58:02 +0530

    Creating or updating file pages/cart_page.py via GitHubFileCreator tool

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -4,7 +4,11 @@
   "pages/dashboard_page.py": "",
   "pages/cart_page.py": "",
   "pages/checkout_page.py": "",
+  "pages/profile_page.py": "",
   "tests/test_login.py": "",
+  "tests/test_search_and_cart.py": "",
+  "tests/test_checkout.py": "",
+  "tests/test_logout.py": "",
   "conftest.py": "",
   "pytest.ini": "",
   "requirements.txt": ""
